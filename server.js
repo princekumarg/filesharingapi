43c90d1ac1e6f51cb4dfe0c12d2817041c257404
prince kumar <agarwal8789@gmail.com>
2023-08-26 18:09:32 +0530

    commit made by prince

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express=require('express');
 const app=express();
 const path=require('path');
 const PORT=process.env.PORT || 3000;
+const cors=require('cors');
 app.use(express.static('public'));
 //express doesn't parese the json data so we have to use this middleware
 app.use(express.json());
 const connectDB=require('./config/db');
 connectDB();
+//cores
+const corsOptions={
+    origin:process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 //Template engine
 app.set('views', path.join(__dirname,'/views'));
 app.set('view engine','ejs');
